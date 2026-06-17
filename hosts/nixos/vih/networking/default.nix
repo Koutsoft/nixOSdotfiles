b604580c075cb5bd80d8b980841ed279e4762c34
Koutsoft <souza.marcondes@outlook.com>
2026-06-17 13:27:51 -0300

    add iperf3

--- hosts/nixos/vih/networking/default.nix
+++ hosts/nixos/vih/networking/default.nix
@@ -1,5 +1,5 @@
 {
-  imports = [ ./firewall.nix ./ssh.nix   ./bluetooth.nix  ];
+  imports = [ ./firewall.nix ./ssh.nix   ./bluetooth.nix ./iperf3.nix ];
   networking = {
     hostName = "vih";
     networkmanager.enable = true;
