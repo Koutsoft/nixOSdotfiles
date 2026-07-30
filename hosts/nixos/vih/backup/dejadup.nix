d0858f48ae2ee773f2ccfd0f8aeea41dfbc027ce
Koutsoft <souza.marcondes@outlook.com>
2026-07-30 12:11:02 -0300

    .

--- hosts/nixos/vih/backup/dejadup.nix
+++ hosts/nixos/vih/backup/dejadup.nix
@@ -3,6 +3,10 @@
 {
   services.gvfs.enable = true;
 
+  services.gnome.gnome-keyring.enable = true;
+
+  security.pam.services.sddm.enableGnomeKeyring = true;
+
   environment.systemPackages = with pkgs; [
     deja-dup
   ];
